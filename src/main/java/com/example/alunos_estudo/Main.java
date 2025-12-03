9312b2d0903eae6b4cf86e6d9d15795a838070cc
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-03 17:16:34 -0300

    first commit

---
+++ src/main/java/com/example/alunos_estudo/Main.java
@@ -0,0 +1,13 @@
+package com.example.alunos_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alunos_estudo/AlunosEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alunos_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
