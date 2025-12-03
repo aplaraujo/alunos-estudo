552bb0f6bd14cf724f9026c0d1ad39561bfbc9be
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-03 17:44:41 -0300

    Criação DTO

---
+++ src/main/java/com/example/alunos_estudo/dto/AlunoRequest.java
@@ -0,0 +1,8 @@
+package com.example.alunos_estudo.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record AlunoRequest(String nome, String telefone, LocalDate dataNascimento, List<MatriculaDTO> matriculas) {
+
+}
---
+++ src/main/java/com/example/alunos_estudo/dto/MatriculaDTO.java
@@ -0,0 +1,7 @@
+package com.example.alunos_estudo.dto;
+
+import java.time.LocalDate;
+
+public record MatriculaDTO(String codigoMatricula, String nomeCurso, LocalDate dataInicio) {
+
+}
